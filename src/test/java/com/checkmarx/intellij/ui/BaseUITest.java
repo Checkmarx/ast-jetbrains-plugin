62119f3778d559c8c41ec3e2e9de25406411ea39
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2021-10-13 09:28:01 +0100

    Raise find duration

--- src/test/java/com/checkmarx/intellij/ui/BaseUITest.java
+++ src/test/java/com/checkmarx/intellij/ui/BaseUITest.java
@@ -71,7 +71,7 @@ public abstract class BaseUITest {
 
     protected static <T extends ComponentFixture> T find(Class<T> cls,
                                                          @Language("XPath") String xpath) {
-        return find(cls, xpath, Duration.ofSeconds(2));
+        return find(cls, xpath, Duration.ofSeconds(30));
     }
 
     protected static <T extends ComponentFixture> T find(Class<T> cls,
