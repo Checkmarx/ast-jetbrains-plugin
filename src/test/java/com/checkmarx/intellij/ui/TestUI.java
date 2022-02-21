93387048a44887bbe0b6133e423f9467fff4b65e
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2022-02-21 17:10:04 +0000

    remove ends-with

--- src/test/java/com/checkmarx/intellij/ui/TestUI.java
+++ src/test/java/com/checkmarx/intellij/ui/TestUI.java
@@ -102,7 +102,7 @@ public class TestUI extends BaseUITest {
     private void findLatestScanSelection() {
         @Language("XPath") String xpath = String.format(
                 "//div[@class='ActionButtonWithText' and substring(@visible_text, string-length(@visible_text) - string-length('%s') + 1)  = '%s']",
-                Utils.formatLatest(true));
+                Utils.formatLatest(true),Utils.formatLatest(true));
         waitFor(() -> hasAnyComponent(xpath));
     }
 
