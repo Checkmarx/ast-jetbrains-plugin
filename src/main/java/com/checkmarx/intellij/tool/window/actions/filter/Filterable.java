61f2cc66baf04cb0f26d3a9e29d01166eef7ce13
Diogo Pimenta <diogo.pimenta@checkmarx.com>
2022-03-23 10:04:16 +0000

    Use null icon instead of EMPTY_0 to properly display the state action menu in 2021.1

--- src/main/java/com/checkmarx/intellij/tool/window/actions/filter/Filterable.java
+++ src/main/java/com/checkmarx/intellij/tool/window/actions/filter/Filterable.java
@@ -1,14 +1,12 @@
 package com.checkmarx.intellij.tool.window.actions.filter;
 
-import com.intellij.util.ui.EmptyIcon;
-
 import javax.swing.*;
 import java.util.function.Supplier;
 
 public interface Filterable {
 
     default Icon getIcon() {
-        return EmptyIcon.ICON_0;
+        return null;
     }
 
     Supplier<String> tooltipSupplier();
