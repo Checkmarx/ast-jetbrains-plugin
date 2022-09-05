d7231c157f3d21a859a5c931b117b3092e3ddc7d
tiagobcx <tiago.baptista@checkmarx.com>
2022-09-05 15:14:00 +0100

    reverting gradle changes

--- gradlew.bat
+++ gradlew.bat
@@ -21,7 +21,7 @@
 @rem
 @rem ##########################################################################
 
-@rem Set§ local scope for the variables with windows NT shell
+@rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
