3167268a9ce2ea5cc816777f0a9713a16598fc27
hmmachadocx <Hugo.Machado@checkmarx.com>
2022-09-30 15:09:53 +0100

    AST-14325 - Change authentication screen
    - Fix ui test

--- src/test/java/com/checkmarx/intellij/standard/BaseTest.java
+++ src/test/java/com/checkmarx/intellij/standard/BaseTest.java
@@ -5,7 +5,6 @@ import com.checkmarx.intellij.Environment;
 import com.checkmarx.intellij.settings.global.GlobalSettingsSensitiveState;
 import com.checkmarx.intellij.settings.global.GlobalSettingsState;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,11 +16,6 @@ public abstract class BaseTest extends BasePlatformTestCase {
         super.setUp();
         GlobalSettingsState state = GlobalSettingsState.getInstance();
         GlobalSettingsSensitiveState sensitiveState = GlobalSettingsSensitiveState.getInstance();
-
-        state.setServerURL(System.getenv("CX_BASE_URI"));
-        if (StringUtils.isNotBlank(System.getenv("CX_TENANT"))) {
-            state.setTenantName(System.getenv("CX_TENANT"));
-        }
         sensitiveState.setApiKey(System.getenv("CX_APIKEY"));
         state.setAdditionalParameters("--debug");
     }
--- src/test/java/com/checkmarx/intellij/standard/commands/TestAuthentication.java
+++ src/test/java/com/checkmarx/intellij/standard/commands/TestAuthentication.java
@@ -5,7 +5,6 @@ import com.checkmarx.intellij.commands.Authentication;
 import com.checkmarx.intellij.settings.global.GlobalSettingsSensitiveState;
 import com.checkmarx.intellij.settings.global.GlobalSettingsState;
 import com.checkmarx.intellij.standard.BaseTest;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,10 +22,6 @@ public class TestAuthentication extends BaseTest {
     public void beforeEach() {
         state = new GlobalSettingsState();
         sensitiveState = new GlobalSettingsSensitiveState();
-        state.setServerURL(System.getenv("CX_BASE_URI"));
-        if (StringUtils.isNotBlank(System.getenv("CX_TENANT"))) {
-            state.setTenantName(System.getenv("CX_TENANT"));
-        }
         sensitiveState.setApiKey(System.getenv("CX_APIKEY"));
     }
 
@@ -42,15 +37,6 @@ public class TestAuthentication extends BaseTest {
     @Test
     public void testSuccessfulConnection() {
         Assertions.assertDoesNotThrow(() -> Authentication.validateConnection(state, sensitiveState));
-    }
-
-    /**
-     * Test failing connection due to wrong server URL.
-     */
-    @Test
-    public void testFailServerURL() {
-        state.setServerURL("https://wronghost.com");
-        Assertions.assertThrows(CxException.class, () -> Authentication.validateConnection(state, sensitiveState));
     }
 
     /**
