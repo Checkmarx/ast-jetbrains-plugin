e873b5e4fa7ba5a8afa5c7570914894630634cfd
hmmachadocx <91129244+hmmachadocx@users.noreply.github.com>
2022-12-12 15:14:17 +0000

    Improve UI tests (#159)
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Improve UI tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add Authentication tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests -> end-to-end
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Add General tests
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code
    
    * AST-18338 - Tech Debt - UI Tests - JetBrains - Refactor UI Tests
    
    - Clean code

--- src/test/java/com/checkmarx/intellij/ui/BaseUITest.java
+++ src/test/java/com/checkmarx/intellij/ui/BaseUITest.java
@@ -1,20 +1,18 @@
 package com.checkmarx.intellij.ui;
 
+import com.checkmarx.intellij.Constants;
 import com.checkmarx.intellij.Environment;
-import com.checkmarx.intellij.tool.window.ResultState;
-import com.checkmarx.intellij.tool.window.Severity;
-import com.intellij.remoterobot.RemoteRobot;
-import com.intellij.remoterobot.fixtures.ComponentFixture;
-import com.intellij.remoterobot.fixtures.JButtonFixture;
-import com.intellij.remoterobot.fixtures.JTextFieldFixture;
-import com.intellij.remoterobot.search.locators.Locators;
+import com.intellij.remoterobot.fixtures.*;
+import com.intellij.remoterobot.fixtures.dataExtractor.RemoteText;
 import com.intellij.remoterobot.stepsProcessing.StepLogger;
 import com.intellij.remoterobot.stepsProcessing.StepWorker;
 import com.intellij.remoterobot.utils.Keyboard;
 import com.intellij.remoterobot.utils.RepeatUtilsKt;
-import com.intellij.remoterobot.utils.UtilsKt;
 import com.intellij.remoterobot.utils.WaitForConditionTimeoutException;
+import org.apache.commons.lang3.StringUtils;
 import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.awt.event.KeyEvent;
@@ -22,53 +20,21 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import static com.checkmarx.intellij.ui.utils.RemoteRobotUtils.*;
+import static com.checkmarx.intellij.ui.utils.Xpath.*;
 
 public abstract class BaseUITest {
-
-    @Language("XPath")
-    protected static final String SETTINGS_ACTION = "//div[@myicon='settings.svg']";
-    @Language("XPath")
-    protected static final String SETTINGS_BUTTON = "//div[@text='Open Settings']";
-    @Language("XPath")
-    protected static final String EXPAND_ACTION = "//div[@tooltiptext='Expand all']";
-    @Language("XPath")
-    protected static final String COLLAPSE_ACTION = "//div[@tooltiptext='Collapse all']";
-    @Language("XPath")
-    protected static final String FILTER_BY_ACTION = "//div[@myicon='filter.svg']";
-    @Language("XPath")
-    protected static final String GROUP_BY_ACTION = "//div[@myicon='groupBy.svg']";
-    @Language("XPath")
-    protected static final String CLONE_BUTTON = "//div[@text='Clone']";
-    @Language("XPath")
-    protected static final String FIELD_NAME = "//div[@name='%s']";
-    @Language("XPath")
-    protected static final String CHANGES_COMMENT = "//div[@accessiblename='%s' and @class='JLabel' and @text='<html>%s</html>']";
-    @Language("XPath")
-    protected static final String VALIDATE_BUTTON = "//div[@class='JButton' and @text='Validate connection']";
-    @Language("XPath")
-    protected static final String STATE_COMBOBOX_ARROW = "//div[@class='ComboBox'][.//div[@visible_text='TO_VERIFY']]//div[@class='BasicArrowButton']|//div[@class='ComboBox'][.//div[@visible_text='CONFIRMED']]//div[@class='BasicArrowButton']|//div[@class='ComboBox'][.//div[@visible_text='URGENT']]//div[@class='BasicArrowButton']";
-    @Language("XPath")
-    protected static final String SEVERITY_COMBOBOX_ARROW = "//div[@class='ComboBox'][.//div[@visible_text='MEDIUM']]//div[@class='BasicArrowButton']|//div[@class='ComboBox'][.//div[@visible_text='HIGH']]//div[@class='BasicArrowButton']|//div[@class='ComboBox'][.//div[@visible_text='LOW']]//div[@class='BasicArrowButton']";
-    @Language("XPath")
-    protected static final String SCAN_FIELD = "//div[@class='TextFieldWithProcessing']";
-    @Language("XPath")
-    protected static final String TREE = "//div[@class='Tree']";
-    @Language("XPath")
-    protected static final String LINK_LABEL = "//div[@class='CxLinkLabel']";
-    @Language("XPath")
-    protected static final String EDITOR = "//div[@class='EditorComponentImpl']";
-    @Language("XPath")
-    protected static final String JLIST = "//div[@class='JList']";
-    @Language("XPath")
-    protected static final String START_SCAN_BTN = "//div[contains(@myaction.key, 'START_SCAN_ACTION')]";
-    @Language("XPath")
-    protected static final String CANCEL_SCAN_BTN = "//div[@myaction.key='CANCEL_SCAN_ACTION']";
-
-    protected static final RemoteRobot remoteRobot = new RemoteRobot("http://127.0.0.1:8580");
 
     protected static final Duration waitDuration = Duration.ofSeconds(Integer.getInteger("uiWaitDuration"));
     private static boolean initialized = false;
     private static int retries = 0;
+    protected static ComponentFixture baseLabel;
+    protected static JTextFieldFixture scanIdTextBox;
+    protected static ActionButtonFixture projectCombobox;
+    protected static ActionButtonFixture branchCombobox;
+    protected static ActionButtonFixture scanCombobox;
 
     @BeforeAll
     public static void init() {
@@ -76,32 +42,38 @@ public abstract class BaseUITest {
             log("Initializing the tests");
             log("Wait duration set for " + waitDuration.getSeconds());
             StepWorker.registerProcessor(new StepLogger());
-            if (hasAnyComponent("//div[@class='FlatWelcomeFrame']")) {
-                find("//div[@defaulticon='fromVCSTab.svg']").click();
-                find(JTextFieldFixture.class, "//div[@class='BorderlessTextField']", Duration.ofSeconds(10))
-                        .setText(Environment.REPO);
+            if (hasAnyComponent(FLAT_WELCOME_FRAME)) {
+                find(FROM_VCS_TAB).click();
+                find(JTextFieldFixture.class, BORDERLESS_TEXT_FIELD, Duration.ofSeconds(10)).setText(Environment.REPO);
                 waitFor(() -> hasAnyComponent(CLONE_BUTTON) && find(JButtonFixture.class, CLONE_BUTTON).isEnabled());
                 find(CLONE_BUTTON).click();
-                try {
-                    waitAndClick("//div[@text='Trust Project']");
-                    waitFor(() -> hasAnyComponent("//div[@class='ContentTabLabel']"));
-                } catch (WaitForConditionTimeoutException e) {
-                    // if exception is thrown, sync was successful, so we can keep going
-                }
+                trustClonedProject();
             }
+            // Open Checkmarx One plugin
+            openCxToolWindow();
+
+            // Resize Checkmarx One plugin so that all toolbar icons are visible
+            resizeToolBar();
+
+            // Connect to AST
+            testASTConnection(true);
+
+            // Store elements to be used during the tests
+            initializeElements();
+
             initialized = true;
             log("Initialization finished");
         } else {
             log("Tests already initialized, skipping");
         }
-        openCxToolWindow();
-        resizeToolBar();
     }
 
-    protected static void resizeToolBar() {
-        click("//div[@class='BaseLabel']");
+    private static void resizeToolBar() {
+        waitFor(() -> hasAnyComponent(BASE_LABEL));
+        baseLabel = find(BASE_LABEL);
         Keyboard keyboard = new Keyboard(remoteRobot);
         for (int i = 0; i < 3; i++) {
+            baseLabel.click();
             if (remoteRobot.isMac()) {
                 keyboard.hotKey(KeyEvent.VK_SHIFT, KeyEvent.VK_META, KeyEvent.VK_UP);
             } else {
@@ -117,53 +89,23 @@ public abstract class BaseUITest {
                 keyboard.backspace();
             }
             keyboard.enterText(value);
-            return hasAnyComponent("//div[@visible_text='" + value + "']");
+            return hasAnyComponent(String.format(VISIBLE_TEXT, value));
         });
         keyboard.enter();
     }
 
-    protected static void click(@Language("XPath") String xpath) {
-        find(xpath).click();
+    private static void trustClonedProject() {
+        try {
+            waitFor(() -> hasAnyComponent(TRUST_PROJECT) && find(TRUST_PROJECT).isShowing());
+            find(TRUST_PROJECT).click();
+        } catch(WaitForConditionTimeoutException ignored) {}
     }
 
-    protected static ComponentFixture find(@Language("XPath") String xpath) {
-        return find(ComponentFixture.class, xpath);
-    }
-
-    protected static <T extends ComponentFixture> T find(Class<T> cls,
-                                                         @Language("XPath") String xpath) {
-        return find(cls, xpath, Duration.ofSeconds(90));
-    }
-
-    protected static <T extends ComponentFixture> T find(Class<T> cls,
-                                                         @Language("XPath") String xpath,
-                                                         Duration duration) {
-        return remoteRobot.find(cls, Locators.byXpath(xpath), duration);
-    }
-
-    protected static List<ComponentFixture> findAll(@Language("XPath") String xpath) {
-        return findAll(ComponentFixture.class, xpath);
-    }
-
-    protected static <T extends ComponentFixture> List<T> findAll(Class<T> cls,
-                                                                  @Language("XPath") String xpath) {
-        return remoteRobot.findAll(cls, Locators.byXpath(xpath));
-    }
-
-    protected static void waitAndClick(@Language("XPath") String xpath) {
-        waitFor(() -> hasAnyComponent(xpath) && find(xpath).isShowing());
-        find(xpath).click();
-    }
-
-    protected void setField(String fieldName, String value) {
+    private static void setField(String fieldName, String value) {
         log("Setting field " + fieldName);
         @Language("XPath") String fieldXpath = String.format(FIELD_NAME, fieldName);
         waitFor(() -> hasAnyComponent(fieldXpath) && find(fieldXpath).isShowing());
         find(JTextFieldFixture.class, String.format(FIELD_NAME, fieldName), waitDuration).setText(value);
-    }
-
-    protected static boolean hasAnyComponent(@Language("XPath") String xpath) {
-        return UtilsKt.hasAnyComponent(remoteRobot, Locators.byXpath(xpath));
     }
 
     protected static void waitFor(Supplier<Boolean> condition) {
@@ -172,7 +114,9 @@ public abstract class BaseUITest {
         }catch(WaitForConditionTimeoutException e) {
             retries++;
             if(retries < 3){
-                click("//div[@class='BaseLabel']");
+                if(baseLabel != null && baseLabel.isShowing()) {
+                    baseLabel.click();
+                }
             } else{
                 retries = 0;
                 throw e;
@@ -180,22 +124,158 @@ public abstract class BaseUITest {
         }
     }
 
-    protected static void openCxToolWindow() {
+    private static void openCxToolWindow() {
         log("Opening Cx Tool Window");
-        @Language("XPath") String xpath = "//div[@text='Checkmarx' and @class='StripeButton']";
-        waitFor(() -> hasAnyComponent(xpath));
+        waitFor(() -> hasAnyComponent(CHECKMARX_STRIPE_BTN));
         if (!(hasAnyComponent(SETTINGS_ACTION) || hasAnyComponent(SETTINGS_BUTTON))) {
-            find(xpath).click();
+            find(CHECKMARX_STRIPE_BTN).click();
         }
     }
 
-    @Language("XPath")
-    protected static String filterXPath(Severity filter) {
-        return String.format("//div[@myicon='%s.svg']", filter.tooltipSupplier().get().toLowerCase());
+    private static void initializeElements() {
+        baseLabel.click();
+        scanIdTextBox = find(JTextFieldFixture.class, SCAN_FIELD);
+        projectCombobox = findSelection("Project");
+        branchCombobox = findSelection("Branch");
+        scanCombobox = findSelection("Scan");
     }
 
     protected static void log(String msg) {
         StackTraceElement[] st = Thread.currentThread().getStackTrace();
         System.out.printf("%s | %s: %s%n", Instant.now().toString(), st[2], msg);
+    }
+
+    protected static void testASTConnection(boolean validCredentials) {
+        openSettings();
+
+        setField(Constants.FIELD_NAME_API_KEY, validCredentials ? Environment.API_KEY : "invalidAPIKey");
+        setField(Constants.FIELD_NAME_ADDITIONAL_PARAMETERS, "--debug");
+
+        click(VALIDATE_BUTTON);
+
+        waitFor(() -> !hasAnyComponent(VALIDATING_CONNECTION));
+
+        if (validCredentials) {
+            Assertions.assertTrue(hasAnyComponent(SUCCESS_CONNECTION));
+            click(OK_BTN);
+            baseLabel.click();
+            // Ensure that start scan button and cancel scan button are visible with valid credentials
+            waitFor(() -> hasAnyComponent(START_SCAN_BTN));
+            waitFor(() -> hasAnyComponent(CANCEL_SCAN_BTN));
+        } else {
+            Assertions.assertFalse(hasAnyComponent(SUCCESS_CONNECTION));
+            click(OK_BTN);
+            baseLabel.click();
+            // Ensure that start scan button and cancel scan button are hidden with invalid credentials
+            waitFor(() -> !hasAnyComponent(START_SCAN_BTN));
+            waitFor(() -> !hasAnyComponent(CANCEL_SCAN_BTN));
+        }
+    }
+
+    private static void openSettings() {
+        waitFor(() -> {
+            if (hasAnyComponent(SETTINGS_ACTION)) {
+                click(SETTINGS_ACTION);
+            } else if (hasAnyComponent(SETTINGS_BUTTON)) {
+                click(SETTINGS_BUTTON);
+            }
+            return hasAnyComponent(String.format(FIELD_NAME, Constants.FIELD_NAME_API_KEY));
+        });
+    }
+
+    protected static void testFileNavigation() {
+        waitFor(() -> {
+            baseLabel.click();
+            findAll(LINK_LABEL).get(0).doubleClick();
+            return hasAnyComponent(EDITOR);
+        });
+        Assertions.assertDoesNotThrow(() -> find(EditorFixture.class, EDITOR, waitDuration));
+        //Confirming if editor is opened
+        find(EditorFixture.class, EDITOR, waitDuration);
+    }
+
+    protected void getResults() {
+        baseLabel.click();
+        waitFor(() -> hasAnyComponent(SCAN_FIELD) && hasSelection("Project") && hasSelection("Branch") && hasSelection("Scan"));
+        baseLabel.click();
+        scanIdTextBox.setText(Environment.SCAN_ID);
+        new Keyboard(remoteRobot).key(KeyEvent.VK_ENTER);
+        waitFor(() -> {
+            baseLabel.click();
+            return hasAnyComponent(String.format("//div[@class='Tree' and contains(@visible_text,'Scan %s')]", Environment.SCAN_ID));
+        });
+    }
+
+    private static boolean hasSelection(String s) {
+        return hasAnyComponent(String.format(HAS_SELECTION, s));
+    }
+
+    protected void waitForScanIdSelection() {
+        baseLabel.click();
+        // check scan selection for the scan id
+        waitFor(() -> hasAnyComponent(String.format(SCAN_ID_SELECTION, Environment.SCAN_ID, Environment.SCAN_ID)));
+    }
+
+    protected void navigate(String prefix, int minExpectedSize) {
+        waitFor(() -> {
+            List<RemoteText> prefixNodes = find(JTreeFixture.class, TREE).getData()
+                    .getAll()
+                    .stream()
+                    .filter(t -> t.getText().startsWith(prefix))
+                    .collect(Collectors.toList());
+            if (prefixNodes.size() == 0) {
+                return false;
+            }
+            prefixNodes.get(0).doubleClick();
+            return find(JTreeFixture.class, TREE).findAllText().size() >= minExpectedSize;
+        });
+    }
+
+    @NotNull
+    private static ActionButtonFixture findSelection(String s) {
+        @Language("XPath") String xpath = String.format(
+                "//div[@class='ActionButtonWithText' and starts-with(@visible_text,'%s: ')]",
+                s);
+        waitFor(() -> hasAnyComponent(xpath));
+        return find(ActionButtonFixture.class, xpath);
+    }
+
+    protected void testSelectionAction(ActionButtonFixture selection, String prefix, String value) {
+        waitFor(() -> {
+            baseLabel.click();
+            System.out.println(selection.getTemplatePresentationText());
+            return selection.isEnabled() && selection.getTemplatePresentationText().contains(prefix);
+        });
+        waitFor(() -> {
+            baseLabel.click();
+            selection.click();
+            List<JListFixture> jListFixtures = findAll(JListFixture.class, MY_LIST);
+
+            return jListFixtures.size() == 1 && jListFixtures.get(0).findAllText().size() > 0;
+        });
+        enter(value);
+    }
+
+    protected void clearSelection() {
+        waitFor(() -> {
+            if (hasAnyComponent(NO_PROJECT_SELECTED)
+                    && projectCombobox.isEnabled()
+                    && hasAnyComponent(NO_BRANCH_SELECTED)
+                    && hasAnyComponent(NO_SCAN_SELECTED)
+                    && !hasAnyComponent(TREE)
+                    && StringUtils.isBlank(scanIdTextBox.getText())) {
+                log("clear selection done");
+                return true;
+            }
+            if (!scanCombobox.isShowing() || (scanCombobox.hasText("Scan: ..."))
+                    || (!branchCombobox.isShowing() || branchCombobox.hasText("Branch: ..."))
+                    || (!projectCombobox.isShowing() || projectCombobox.hasText("Project: ..."))) {
+                log("clear selection still in progress");
+                return false;
+            }
+            log("clicking refresh action button");
+            click(CLEAR_BTN);
+            return false;
+        });
     }
 }
---
+++ src/test/java/com/checkmarx/intellij/ui/TestAuthentication.java
@@ -0,0 +1,16 @@
+package com.checkmarx.intellij.ui;
+
+import com.automation.remarks.junit5.Video;
+import org.junit.jupiter.api.Test;
+
+public class TestAuthentication extends BaseUITest{
+    @Test
+    @Video
+    public void testASTAuthentication() {
+        // Test wrong connection
+        testASTConnection(false);
+
+        // Test successfully connection
+        testASTConnection(true);
+    }
+}
