1698d91651cb21f123b36e58e62ab55325d87475
igorlombacx <igor.lomba@checkmarx.com>
2023-02-28 18:59:28 +0000

    AST-18947 - Additional Parameter info tooltip

--- src/main/java/com/checkmarx/intellij/settings/global/GlobalSettingsComponent.java
+++ src/main/java/com/checkmarx/intellij/settings/global/GlobalSettingsComponent.java
@@ -169,7 +169,9 @@ public class GlobalSettingsComponent implements SettingsComponent {
         mainPanel.add(new JBLabel());
         mainPanel.add(CxLinkLabel.buildDocLinkLabel(Constants.ADDITIONAL_PARAMETERS_HELP, Resource.HELP_CLI),
                       "gapleft 5, wrap");
-
+        mainPanel.add(new JLabel());
+        mainPanel.add(CxLinkLabel.buildDocLinkLabel(Constants.ADDITIONAL_PARAMETERS_HELP_CLI_GLOBAL_FLAGS, Resource.HELP_CLI_GLOBAL_FLAGS),
+                "gapleft 5, wrap");
         mainPanel.add(validateButton, "sizegroup bttn, gaptop 30");
         mainPanel.add(validateResult, "gapleft 5, gaptop 30");
     }
