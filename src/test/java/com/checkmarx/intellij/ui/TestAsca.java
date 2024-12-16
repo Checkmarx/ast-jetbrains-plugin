617e03446322fb1c4490bc8be9905979d96407dd
AlvoBen <alvo@post.bgu.ac.il>
2024-12-16 16:57:13 +0200

    add ui test

--- src/test/java/com/checkmarx/intellij/ui/TestAsca.java
+++ src/test/java/com/checkmarx/intellij/ui/TestAsca.java
@@ -3,6 +3,7 @@ package com.checkmarx.intellij.ui;
 import com.automation.remarks.junit5.Video;
 import com.intellij.remoterobot.fixtures.JTreeFixture;
 import com.intellij.remoterobot.search.locators.Locators;
+import com.intellij.remoterobot.utils.Keyboard;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -40,5 +41,38 @@ public class TestAsca extends BaseUITest {
         click(OK_BTN);
         validateAscaRunning();
         click(OK_BTN);
+    }
+
+    @Test
+    @Video
+    public void testGetVul() throws InterruptedException {
+        clickAscaCheckbox();
+        click(OK_BTN);
+        click("//div[contains(@tooltiptext.key, 'title.project')]");
+
+        enter("webgoat");
+        enter("webgoat-lessons");
+        enter("challenge");
+        enter("src");
+        enter("main");
+        enter("java");
+        enter("challenge5");
+        enter("Assignment5");
+
+        click("//div[@text.key='toolwindow.stripe.Problems_View']");
+        click("//div[@class='BaseLabel' and @text='Problems:']");
+        enter("ASCA");
+        enter("Unsafe SQL Query Construction");
+
+        waitFor(() -> hasAnyComponent("//div[contains(@mytext, 'Unsafe SQL Query Construction - Consider using prepared statements instead of concatenation when building SQL statement.')]"));
+    }
+
+    protected static void enter(String value) {
+        Keyboard keyboard = new Keyboard(remoteRobot);
+        waitFor(() -> {
+            keyboard.enterText(value);
+            return hasAnyComponent(String.format(VISIBLE_TEXT, value));
+        });
+        keyboard.enter();
     }
 }
