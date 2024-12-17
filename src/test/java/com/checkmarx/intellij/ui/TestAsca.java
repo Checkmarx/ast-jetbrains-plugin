2e0a2039c08a2222cd572997e79365b3d20001b2
AlvoBen <alvo@post.bgu.ac.il>
2024-12-17 15:32:23 +0200

    refactor

--- src/test/java/com/checkmarx/intellij/ui/TestAsca.java
+++ src/test/java/com/checkmarx/intellij/ui/TestAsca.java
@@ -16,7 +16,7 @@ import static com.checkmarx.intellij.ui.utils.Xpath.*;
 
 public class TestAsca extends BaseUITest {
 
-    public void clickAscaCheckbox(){
+    public void clickAscaCheckbox() {
         openSettings();
         waitFor(() -> hasAnyComponent(ASCA_CHECKBOX));
         click(ASCA_CHECKBOX);
@@ -24,7 +24,7 @@ public class TestAsca extends BaseUITest {
         Assertions.assertTrue(hasAnyComponent(ASCA_INSTALL_SUCCESS));
     }
 
-    public void validateAscaRunning(){
+    public void validateAscaRunning() {
         openSettings();
         waitFor(() -> hasAnyComponent(ASCA_INSTALL_SUCCESS));
         Assertions.assertTrue(hasAnyComponent(ASCA_INSTALL_SUCCESS));
@@ -47,42 +47,42 @@ public class TestAsca extends BaseUITest {
         click(OK_BTN);
     }
 
-@Test
-@Video
-public void AscaCheckboxEnabled_EnteringFileWithVulnerabilities_AscaVulnerabilityExist() {
-    // Attempt to find and click the project side tab button
-    ComponentFixture projectSideTabButton = find(ComponentFixture.class, "//div[contains(@tooltiptext.key, 'title.project')]", waitDuration);
-    try {
-        // We assume that project side tab is open, trying to click the project view tree
-        ComponentFixture projectViewTree = find(ComponentFixture.class, "//div[@class='JBViewport'][.//div[@class='ProjectViewTree']]", Duration.ofSeconds(1));
-        Point webGoatRootDirectoryPoint = projectViewTree.findAllText().get(0).getPoint();
-        projectViewTree.click(webGoatRootDirectoryPoint);
-    } catch (Exception e) {
-        // If the project side tab button is not open, click the project side tab button
-        projectSideTabButton.click();
+    @Test
+    @Video
+    public void AscaCheckboxEnabled_EnteringFileWithVulnerabilities_AscaVulnerabilityExist() {
+        // Attempt to find and click the project side tab button
+        ComponentFixture projectSideTabButton = find(ComponentFixture.class, "//div[contains(@tooltiptext.key, 'title.project')]", waitDuration);
+        try {
+            // We assume that project side tab is open, trying to click the project view tree
+            ComponentFixture projectViewTree = find(ComponentFixture.class, "//div[@class='JBViewport'][.//div[@class='ProjectViewTree']]", Duration.ofSeconds(1));
+            Point webGoatRootDirectoryPoint = projectViewTree.findAllText().get(0).getPoint();
+            projectViewTree.click(webGoatRootDirectoryPoint);
+        } catch (Exception e) {
+            // If the project side tab button is not open, click the project side tab button
+            projectSideTabButton.click();
+        }
+
+        // Navigate through the project directory to the specific file path
+        String[] path = {"webgoat-lessons", "challenge", "src", "main", "java", "challenge5", "Assignment5"};
+        for (String step : path) {
+            enter(step);
+        }
+
+        // Open the Problems view and search for a specific problem
+        click("//div[contains(@text.key, 'toolwindow.stripe.Problems_View')]");
+        click("//div[@class='BaseLabel' and @text='Problems:']");
+
+        ComponentFixture problems = find(ComponentFixture.class, "//div[@class='Tree']", waitDuration);
+
+        waitFor(() -> {
+            List<RemoteText> textList = problems.findAllText();
+            return textList.stream().anyMatch(t -> t.getText().contains("ASCA"));
+        });
+
+        Assertions.assertTrue(problems.findAllText().stream().anyMatch(t -> t.getText().contains("ASCA")));
+
+        openCxToolWindow();
     }
-
-    // Navigate through the project directory to the specific file path
-    String[] path = {"webgoat-lessons", "challenge", "src", "main", "java", "challenge5", "Assignment5"};
-    for (String step : path) {
-        enter(step);
-    }
-
-    // Open the Problems view and search for a specific problem
-    click("//div[contains(@text.key, 'toolwindow.stripe.Problems_View')]");
-    click("//div[@class='BaseLabel' and @text='Problems:']");
-
-    ComponentFixture problems = find(ComponentFixture.class, "//div[@class='Tree']", waitDuration);
-
-    waitFor(() -> {
-        List<RemoteText> textList = problems.findAllText();
-        return textList.stream().anyMatch(t -> t.getText().contains("ASCA"));
-    });
-
-    Assertions.assertTrue(problems.findAllText().stream().anyMatch(t -> t.getText().contains("ASCA")));
-
-    openCxToolWindow();
-}
 
     protected static void enter(String value) {
         Keyboard keyboard = new Keyboard(remoteRobot);
