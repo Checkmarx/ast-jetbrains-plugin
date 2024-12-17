3850033ae8763721991faf2196cbe7b60daf5371
AlvoBen <alvo@post.bgu.ac.il>
2024-12-17 10:46:22 +0200

    upgrade gradle and fix ui test

--- src/test/java/com/checkmarx/intellij/ui/TestAsca.java
+++ src/test/java/com/checkmarx/intellij/ui/TestAsca.java
@@ -1,14 +1,22 @@
 package com.checkmarx.intellij.ui;
 
 import com.automation.remarks.junit5.Video;
+import com.intellij.remoterobot.fixtures.ComponentFixture;
+import com.intellij.remoterobot.fixtures.EditorFixture;
 import com.intellij.remoterobot.fixtures.JTreeFixture;
+import com.intellij.remoterobot.fixtures.dataExtractor.RemoteText;
+import com.intellij.remoterobot.search.locators.Locator;
 import com.intellij.remoterobot.search.locators.Locators;
 import com.intellij.remoterobot.utils.Keyboard;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.List;
+
 import static com.checkmarx.intellij.ui.utils.RemoteRobotUtils.*;
 import static com.checkmarx.intellij.ui.utils.Xpath.*;
+import static com.intellij.remoterobot.search.locators.Locators.byXpath;
 
 public class TestAsca extends BaseUITest {
 
@@ -43,29 +51,31 @@ public class TestAsca extends BaseUITest {
         click(OK_BTN);
     }
 
-    @Test
-    @Video
-    public void testGetVul() throws InterruptedException {
-        clickAscaCheckbox();
-        click(OK_BTN);
-        click("//div[contains(@tooltiptext.key, 'title.project')]");
+@Test
+@Video
+public void testGetVul() {
+    clickAscaCheckbox();
+    click(OK_BTN);
 
-        enter("webgoat");
-        enter("webgoat-lessons");
-        enter("challenge");
-        enter("src");
-        enter("main");
-        enter("java");
-        enter("challenge5");
-        enter("Assignment5");
-
-        click("//div[@text.key='toolwindow.stripe.Problems_View']");
-        click("//div[@class='BaseLabel' and @text='Problems:']");
-        enter("ASCA");
-        enter("Unsafe SQL Query Construction");
-
-        waitFor(() -> hasAnyComponent("//div[contains(@mytext, 'Unsafe SQL Query Construction - Consider using prepared statements instead of concatenation when building SQL statement.')]"));
+    ComponentFixture stripeButton = find(ComponentFixture.class, "//div[contains(@tooltiptext.key, 'title.project')]", waitDuration);
+    try {
+        ComponentFixture stripeButton1 = find(ComponentFixture.class, "//div[@class='JBViewport'][.//div[@class='ProjectViewTree']]", Duration.ofSeconds(1));
+        stripeButton1.click(stripeButton1.findAllText().get(0).getPoint());
+    } catch (Exception e) {
+        stripeButton.click();
     }
+
+    String[] path = {"webgoat-lessons", "challenge", "src", "main", "java", "challenge5", "Assignment5"};
+    for (String step : path) {
+        enter(step);
+    }
+
+    click("//div[@text.key='toolwindow.stripe.Problems_View']");
+    click("//div[@class='BaseLabel' and @text='Problems:']");
+    enter("Unsafe SQL Query Construction");
+
+    waitFor(() -> hasAnyComponent("//div[contains(@mytext, 'Unsafe SQL Query Construction - Consider using prepared statements instead of concatenation when building SQL statement.')]"));
+}
 
     protected static void enter(String value) {
         Keyboard keyboard = new Keyboard(remoteRobot);
