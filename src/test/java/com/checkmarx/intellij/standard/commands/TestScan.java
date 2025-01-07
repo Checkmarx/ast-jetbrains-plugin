a9d648e7d8d45db9b4371125c57eab86e6e60b7b
AlvoBen <alvo@post.bgu.ac.il>
2025-01-07 14:06:35 +0200

    resolve conversations

--- src/test/java/com/checkmarx/intellij/standard/commands/TestScan.java
+++ src/test/java/com/checkmarx/intellij/standard/commands/TestScan.java
@@ -27,6 +27,8 @@ public class TestScan extends BaseTest {
         String msg = String.format("project: %s branch: %s scans: %d", project.getId(), Environment.BRANCH_NAME, scans.size());
         Assertions.assertTrue(scans.size() > 0, msg);
         Assertions.assertTrue(scans.size() <= 10000, msg);
+
+        // Check that all scans are for the correct project and branch and have completed
         Assertions.assertTrue(scans.stream().allMatch(scan -> scan.getProjectId().equals(project.getId()) && scan.getBranch().equals(Environment.BRANCH_NAME) && scan.getStatus().equals("Completed")));
     }
 
