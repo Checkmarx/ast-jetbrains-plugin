fbc8c257f585867d0f2b0195328880d814cbdcc5
AlvoBen <alvo@post.bgu.ac.il>
2025-01-28 16:01:04 +0200

    changed from cx-mac to cx-linux

--- .github/scripts/extract_cli_version.sh
+++ .github/scripts/extract_cli_version.sh
@@ -16,22 +16,22 @@ echo "Found JAR at: $JAR_PATH"
 TEMP_DIR=$(mktemp -d)
 echo "Using temporary directory: $TEMP_DIR"
 
-unzip -j "$JAR_PATH" "cx-mac" -d "$TEMP_DIR"
+unzip -j "$JAR_PATH" "cx-linux" -d "$TEMP_DIR"
 if [ $? -ne 0 ]; then
     echo "Error: Failed to unzip cx-linux from the JAR."
     exit 1
 fi
 
-if [ ! -f "$TEMP_DIR/cx-mac" ]; then
+if [ ! -f "$TEMP_DIR/cx-linux" ]; then
     echo "Error: cx-linux not found inside the JAR."
     ls -la "$TEMP_DIR"
     exit 1
 fi
 
-chmod +x "$TEMP_DIR/cx-mac"
+chmod +x "$TEMP_DIR/cx-linux"
 
 # Extract the CLI version
-CLI_VERSION=$("$TEMP_DIR/cx-mac" version | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+')
+CLI_VERSION=$("$TEMP_DIR/cx-linux" version | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+')
 
 if [ -z "$CLI_VERSION" ]; then
     echo "Error: CLI_VERSION is not set or is empty."
