c30a4a4ad9ec5372ab9ccca95338f2881faa4829
cx-hitesh-madgulkar <212497904+cx-hitesh-madgulkar@users.noreply.github.com>
2025-07-11 12:34:31 +0530

    increased-project-list-to-20K
    Signed-off-by: cx-hitesh-madgulkar <212497904+cx-hitesh-madgulkar@users.noreply.github.com>

--- src/main/java/com/checkmarx/intellij/commands/Project.java
+++ src/main/java/com/checkmarx/intellij/commands/Project.java
@@ -23,7 +23,7 @@ public class Project {
             InterruptedException,
             CxException {
 
-        return CxWrapperFactory.build().projectList("limit=10000");
+        return CxWrapperFactory.build().projectList("limit=20000");
     }
 
     public static List<String> getBranches(@NonNull UUID projectId, boolean isSCMProject)
