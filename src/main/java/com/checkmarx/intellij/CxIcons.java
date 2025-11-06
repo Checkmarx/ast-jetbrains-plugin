25939f94eb2b81c201c707078371108e1da46f95
Aniket Shinde <aniket.shinde@checkmarx.com>
2025-11-06 14:49:18 +0530

    Icons theme based

--- src/main/java/com/checkmarx/intellij/CxIcons.java
+++ src/main/java/com/checkmarx/intellij/CxIcons.java
@@ -15,12 +15,6 @@ public final class CxIcons {
     public static final Icon CHECKMARX_13 = IconLoader.getIcon("/icons/checkmarx-mono-13.png", CxIcons.class);
     public static final Icon CHECKMARX_13_COLOR = IconLoader.getIcon("/icons/checkmarx-13.png", CxIcons.class);
     public static final Icon CHECKMARX_80 = IconLoader.getIcon("/icons/checkmarx-80.png", CxIcons.class);
-    public static final Icon MALICIOUS = IconLoader.getIcon("/icons/malicious.svg", CxIcons.class);
-    public static final Icon CRITICAL = IconLoader.getIcon("/icons/critical.svg", CxIcons.class);
-    public static final Icon HIGH = IconLoader.getIcon("/icons/high.svg", CxIcons.class);
-    public static final Icon MEDIUM = IconLoader.getIcon("/icons/medium.svg", CxIcons.class);
-    public static final Icon LOW = IconLoader.getIcon("/icons/low.svg", CxIcons.class);
-    public static final Icon INFO = IconLoader.getIcon("/icons/info.svg", CxIcons.class);
     public static final Icon COMMENT = IconLoader.getIcon("/icons/comment.svg", CxIcons.class);
     public static final Icon STATE = IconLoader.getIcon("/icons/Flags.svg", CxIcons.class);
     public static final Icon ABOUT = IconLoader.getIcon("/icons/about.svg", CxIcons.class);
@@ -29,4 +23,25 @@ public final class CxIcons {
     public static final Icon WELCOME_MCP_DISABLE = IconLoader.getIcon("/icons/cxAIError.svg", CxIcons.class);
     public static final Icon WELCOME_CHECK = IconLoader.getIcon("/icons/tabler-icon-check.svg", CxIcons.class);
     public static final Icon WELCOME_UNCHECK = IconLoader.getIcon("/icons/tabler-icon-uncheck.svg", CxIcons.class);
+
+    public static Icon getMaliciousIcon() {
+        return IconLoader.getIcon("/icons/malicious.svg", CxIcons.class);
+    }
+    public static Icon getCriticalIcon() {
+        return IconLoader.getIcon("/icons/critical.svg", CxIcons.class);
+    }
+    public static Icon getHighIcon() {
+        return IconLoader.getIcon("/icons/high.svg", CxIcons.class);
+    }
+    public static Icon getMediumIcon() {
+        return IconLoader.getIcon("/icons/medium.svg", CxIcons.class);
+    }
+
+    public static Icon getLowIcon() {
+        return IconLoader.getIcon("/icons/low.svg", CxIcons.class);
+    }
+    public static Icon getInfoIcon() {
+        return IconLoader.getIcon("/icons/info.svg", CxIcons.class);
+    }
+
 }
