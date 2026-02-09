76401c9ff20d9a95ac29852dc683a3ddf595f836
shubhamyadavCx <199061586+cx-shubham-yadav@users.noreply.github.com>
2026-02-09 14:21:13 +0530

    adding project filter tag in project list

--- src/main/java/com/checkmarx/intellij/commands/Project.java
+++ src/main/java/com/checkmarx/intellij/commands/Project.java
@@ -23,7 +23,7 @@ public class Project {
             InterruptedException,
             CxException {
 
-        return CxWrapperFactory.build().projectList("limit=10000");
+        return CxWrapperFactory.build().projectList("limit=10000,tags-keys=SypherUIAutomationTag");
     }
 
     public static List<String> getBranches(@NonNull UUID projectId, boolean isSCMProject)
